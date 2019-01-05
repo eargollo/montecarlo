6a42c7bbb802a0de5c0cb8187c2ebeceebd22ac3
Eduardo Argollo <eduardo@hp.com>
2019-01-04 16:47:16 -0800

    Checking for the file before running simulation to avoid panic error

--- cmd/predict.go
+++ cmd/predict.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.azc.ext.hp.com/eduardo/montecarlo/simulation"
 	"github.com/spf13/cobra"
 )
@@ -12,7 +15,7 @@ var csv bool
 
 func init() {
 	rootCmd.AddCommand(estimateCmd)
-	estimateCmd.PersistentFlags().StringVar(&baseFile, "input", "./input.csv", "Input data, one value per line")
+	estimateCmd.PersistentFlags().StringVar(&baseFile, "input", "input.txt", "Input data, one value per line")
 	estimateCmd.PersistentFlags().IntVar(&future, "future", 12, "Future data points")
 	estimateCmd.PersistentFlags().IntVar(&simulations, "simulations", 1000000, "Amount of MonteCarlo simulations used (the bigger the number, the better the precision but it may take longer to simulate).")
 	estimateCmd.PersistentFlags().Float64Var(&increment, "increment", 5, "Percentual increment for each confidence data point. Default is 5, i.e. one data point for each 5%: 100%, 95%, 90%,...0%")
@@ -24,6 +27,11 @@ var estimateCmd = &cobra.Command{
 	Short: "Simulate future estimation",
 	Long:  `Estimate towards the future based on past data`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := os.Stat(baseFile); os.IsNotExist(err) {
+			// File does not exist
+			fmt.Printf("File %v could not be found. Make sure either the default input.txt file exists or pass your file with the option '--input my-file.txt'", baseFile)
+			return
+		}
 		sim, err := simulation.New(baseFile, future, simulations, increment)
 		if err != nil {
 			panic(err)
